e19a00010ff457e4cffaf8643c61d8ec85f5e296
linkding <beninaoin@gmail.com>
2018-08-08 22:16:28 +0800

    fix bug

--- src/page/MeSetting.vue
+++ src/page/MeSetting.vue
@@ -6,41 +6,68 @@
                 <SideNav/>
             </div>
             <div class="col-lg-6">
-                <h2>常规设置</h2>
                 <div>
-                    <div class="cute-form">
-                        <form v-if="show.username" @submit.prevent="submit('username')">
-                            <input type="text" autofocus v-model="current.username">
-                            <button class="btn" type="submit">确定</button>
-                            <button class="btn" @click="show.username = false" type="button">取消</button>
-                        </form>
-                        <div v-else class="wrap-row">
-                            <div class="col-lg-2">
-                                <span class="key">用户名</span>
+                    <h2>常规设置</h2>
+                    <div>
+                        <div class="cute-form">
+                            <form v-if="show.username" @submit.prevent="submit('username')">
+                                <input type="text" autofocus v-model="current.username">
+                                <button class="btn" type="submit">确定</button>
+                                <button class="btn" @click="show.username = false" type="button">取消</button>
+                            </form>
+                            <div v-else class="wrap-row">
+                                <div class="col-lg-2">
+                                    <span class="key">用户名</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span class="value">{{current.username||'-'}}</span>
+                                </div>
+                                <div class="col-lg-2">
+                                    <button @click="show.username = true" class="btn">编辑</button>
+                                </div>
                             </div>
-                            <div class="col-lg-8">
-                                <span class="value">{{current.username||'-'}}</span>
-                            </div>
-                            <div class="col-lg-2">
-                                <button @click="show.username = true" class="btn">编辑</button>
+                        </div>
+                        <div class="cute-form">
+                            <form v-if="show.email" @submit.prevent="submit(email)">
+                                <input type="text" autofocus v-model="current.email">
+                                <button class="btn" type="submit">确定</button>
+                                <button class="btn" @click="show.email = false" type="button">取消</button>
+                            </form>
+                            <div v-else class="wrap-row">
+                                <div class="col-lg-2">
+                                    <span class="key">邮箱</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span class="value">{{current.email||'-'}}</span>
+                                </div>
+                                <div class="col-lg-2">
+                                    <button @click="show.email = true" class="btn">编辑</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                     <div class="cute-form">
-                        <form v-if="show.email" @submit.prevent="submit(email)">
-                            <input type="text" autofocus v-model="current.email">
-                            <button class="btn" type="submit">确定</button>
-                            <button class="btn" @click="show.email = false" type="button">取消</button>
-                        </form>
-                        <div v-else class="wrap-row">
-                            <div class="col-lg-2">
-                                <span class="key">邮箱</span>
-                            </div>
-                            <div class="col-lg-8">
-                                <span class="value">{{current.email||'-'}}</span>
-                            </div>
-                            <div class="col-lg-2">
-                                <button @click="show.email = true" class="btn">编辑</button>
+                </div>
+                
+                <div>
+                    <h2>安全设置</h2>
+                    <div>
+                        <div class="cute-form">
+                            <form v-if="show.password" @submit.prevent="change_password('password')">
+                                <input type="password" autofocus v-model="password.old" placeholder="原密码">
+                                <input type="password" autofocus v-model="password.new" placeholder="新密码">
+                                <button class="btn" type="submit">确定</button>
+                                <button class="btn" @click="show.password = false" type="button">取消</button>
+                            </form>
+                            <div v-else class="wrap-row">
+                                <div class="col-lg-2">
+                                    <span class="key">密码</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span class="value">{{current.password||'-'}}</span>
+                                </div>
+                                <div class="col-lg-2">
+                                    <button @click="show.password = true" class="btn">编辑</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -64,6 +91,11 @@
                 show:{
                     username:false,
                     email:false,
+                    password:false,
+                },
+                password:{
+                    new:'',
+                    old:'',
                 }
             }
         },
@@ -74,13 +106,16 @@
                         session.replace_uinfo(r.data);
                         this.show[property] = false;
                     })
+            },
+            change_password(property){
+
             }
         }
     }
 </script>
 <style scoped>
 .cute-form{
-    padding: 7px;
+    padding: 15px 7px;
     border-bottom: 1px solid rgba(0, 0, 0, .1)
 }
 .cute-form form >* {
